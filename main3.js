d183f1a646a76cfc95844ce8d8ab899895a08021
Abhin <abhinprakash699@gmail.com>
2024-09-25 13:41:44 +0530

    initial commit for main3.js

---
+++ main3.js
@@ -0,0 +1,16 @@
+// to find length of a string
+
+const l = 'hello world!';
+console.log(l.length);
+
+
+// to select sub-string & convert it into uppercase
+
+const a = 'hello world!';
+console.log(a.toUpperCase().substring(0,5));
+
+
+// to split string
+
+const s = 'computer , phone , tv , speaker';
+console.log(s.split(','));
