a686ac4f7095c9e16376f9845b873602fe53e8e9
Abhin <abhinprakash699@gmail.com>
2024-09-25 13:33:54 +0530

    initial commit for main1.js

---
+++ main1.js
@@ -0,0 +1,12 @@
+// string , numbers , boolean , null  , undefined
+
+
+const name = 'Johnny';
+const age = 30;
+const isCool = true;
+const rating = 5.5;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(age)
