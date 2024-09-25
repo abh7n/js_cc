6dc0b0f3412eaa7ba891f411b75addb7d592a3dc
Abhin <abhinprakash699@gmail.com>
2024-09-25 13:35:21 +0530

    initial commit for main2.js

---
+++ main2.js
@@ -0,0 +1,13 @@
+// concatenation
+
+const Name = 'Abhin';
+const Age = 21;
+
+console.log('My name is ' + Name + ' and I am ' + Age);
+
+// Template string
+
+const name = 'Abhin';
+const age = 21;
+const hello = `My name is ${name} and I am ${age}`;
+console.log(hello);
