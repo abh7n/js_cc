32574bcdffdb374a3d0c2a6c31d7eb73317a6006
Abhin <abhinprakash699@gmail.com>
2024-09-26 19:12:55 +0530

    initial commit for main7.js

---
+++ main7.js
@@ -0,0 +1,15 @@
+// loops
+
+// for 
+for (let i = 0; i <= 10; i++) {
+    console.log(`For loop Number: ${i}`);
+}
+
+
+// while
+let i = 0
+while (i < 10) {
+    console.log(`While Loop Number: ${i}`);
+    i++;
+}
+
