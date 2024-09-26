26b98d4042f04d0a7cf31676e6c23e2b4a766b73
Abhin <abhinprakash699@gmail.com>
2024-09-26 19:15:05 +0530

    initial commit for main8.js

---
+++ main8.js
@@ -0,0 +1,21 @@
+const todos = [
+    {
+        id : 1 ,
+        text : 'Meet with friends' ,
+        isCompleted : true 
+    }
+     ,
+    {
+        id : 2 ,
+        text : 'Go to gym' ,
+        isCompleted : true 
+    } 
+     ,
+    {
+        id : 3 ,
+        text : 'Go to Cinema Theatre' ,
+        isCompleted : false 
+    } 
+]
+
+// forEach , map , filter
