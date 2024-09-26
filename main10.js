2c105dc931165e27bf5753b288690493fe31f186
Abhin <abhinprakash699@gmail.com>
2024-09-26 19:22:12 +0530

    initial commit for main10.js

---
+++ main10.js
@@ -0,0 +1,23 @@
+// adding 2 numbers
+
+function addnums( num1 , num2) {
+    console.log(num1 + num2)
+}
+addnums(3,5);
+
+
+
+ function addNums(numa , numb){
+    return numa + numb ;
+ }
+ console.log(addNums(5,5));
+
+
+const addNum = (nume , numr) => {
+    console.log(nume + numr);
+}
+addNum(5,6);
+
+
+const addno = numx => numx + 5;
+console.log(addno(5));
